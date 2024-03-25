4b5e598e4e28bb1bb977f7667bcb308b022ff05e
eam766 <albertemilie03@gmail.com>
2024-03-24 22:09:08 -0400

    ListCity

--- app/src/main/java/com/example/projet_01/MainActivity.kt
+++ app/src/main/java/com/example/projet_01/MainActivity.kt
@@ -13,20 +13,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -68,7 +76,9 @@ fun DisplayWeather(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,) {
         Spacer(Modifier.height(20.dp))
         SearchBar()
-        Spacer(Modifier.height(70.dp))
+        Spacer(Modifier.height(30.dp))
+        ListCity()
+        Spacer(Modifier.height(60.dp))
         Text(
             text = currentDate,
             modifier = modifier
@@ -103,6 +113,7 @@ fun IconWeather(modifier: Modifier = Modifier){
 @Composable
 fun SearchBar(modifier: Modifier = Modifier){
     TextField(
+        modifier = Modifier.width(350.dp),
         value = "",
         onValueChange = {/*TODO*/},
         label = { Text(text = "Ville: ")},
@@ -118,7 +129,7 @@ fun SearchBar(modifier: Modifier = Modifier){
            )
         },
         trailingIcon={
-            androidx.compose.material3.Icon(
+            Icon(
                 Icons.Default.Clear,
                 contentDescription = "clear text",
                 modifier = Modifier.clickable {
@@ -133,6 +144,47 @@ fun SearchBar(modifier: Modifier = Modifier){
 @Composable
 fun ListCity(modifier: Modifier = Modifier){
     val liste : List<String> = listOf("Montréal", "Laval", "Québec", "Position actuelle")
+    var ouvert = rememberSaveable { mutableStateOf(false)}
+    TextField(
+        modifier = Modifier.width(350.dp),
+        value = "",
+        onValueChange = {/*TODO*/},
+        label = { Text(text = "Ville: ")},
+        shape = RoundedCornerShape(20.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        trailingIcon = {
+            BoutonList(ouvert.value, onClick = { ouvert.value = !ouvert.value})
+        }
+        )
+    if(ouvert.value){
+        LazyColumn(Modifier.background(Color.Gray).width(350.dp)){
+
+            items(liste){
+                item -> Text(item, modifier = modifier.padding(4.dp))
+            }
+        }
+    }
+
+}
+
+@Composable
+fun BoutonList(ouvert: Boolean,
+               onClick: () -> Unit,
+               modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ){
+        Icon(
+        imageVector = if (ouvert) Icons.Filled.ExpandLess else Icons
+            .Filled.ExpandMore,
+        contentDescription = "Ouvrir"
+    )
+    }
 
 }
 
