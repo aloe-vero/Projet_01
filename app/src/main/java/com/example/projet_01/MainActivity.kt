55d5ce9500b9fad656c59b00dd62b6bfa3785d9e
eam766 <albertemilie03@gmail.com>
2024-03-25 13:21:44 -0400

    travail classe

--- app/src/main/java/com/example/projet_01/MainActivity.kt
+++ app/src/main/java/com/example/projet_01/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.projet_01
 
-import android.graphics.drawable.Icon
+import android.content.ContentValues.TAG
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -10,39 +11,30 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -125,10 +117,11 @@ fun IconWeather(modifier: Modifier = Modifier){
 @RequiresApi(Build.VERSION_CODES.M)
 @Composable
 fun SearchBar(modifier: Modifier = Modifier){
+    var ville = rememberSaveable {mutableStateOf("")}
     TextField(
         modifier = Modifier.width(350.dp),
-        value = "",
-        onValueChange = {/*TODO*/},
+        value = ville.value,
+        onValueChange = {ville.value = it},
         label = { Text(text = "Ville: ",
             style = MaterialTheme.typography.bodyMedium,)},
         shape = RoundedCornerShape(20.dp),
@@ -139,7 +132,11 @@ fun SearchBar(modifier: Modifier = Modifier){
         leadingIcon = {
            Icon(
                imageVector = Icons.Filled.Search,
-               contentDescription = "search"
+               contentDescription = "search",
+               modifier = Modifier.clickable {
+                   Log.d(TAG, "button clicked")
+                   Log.i("data: ", ville.value)
+               }
            )
         },
         trailingIcon={
@@ -147,7 +144,7 @@ fun SearchBar(modifier: Modifier = Modifier){
                 Icons.Default.Clear,
                 contentDescription = "clear text",
                 modifier = Modifier.clickable {
-                    /*TODO*/
+                    ville.value = ""
                 }
             )
         }
@@ -155,51 +152,50 @@ fun SearchBar(modifier: Modifier = Modifier){
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListCity(modifier: Modifier = Modifier){
-    val liste : List<String> = listOf("Montréal", "Laval", "Québec", "Position actuelle")
-    var ouvert = rememberSaveable { mutableStateOf(false)}
-    TextField(
-        modifier = Modifier.width(350.dp),
-        value = "",
-        onValueChange = {/*TODO*/},
-        label = { Text(text = "Ville: ")},
-        shape = RoundedCornerShape(20.dp),
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
-        ),
-        trailingIcon = {
-            BoutonList(ouvert.value, onClick = { ouvert.value = !ouvert.value})
-        }
-        )
-    if(ouvert.value){
-        LazyColumn(Modifier.background(Color.Gray).width(350.dp)){
+fun ListCity(modifier: Modifier = Modifier) {
+    val liste: List<String> = listOf("Montréal", "Laval", "Québec", "Position actuelle")
+    var ouvert = rememberSaveable { mutableStateOf(false) }
+    var selectedOptionText = rememberSaveable { mutableStateOf(liste[0]) }
 
-            items(liste){
-                item -> Text(item, modifier = modifier.padding(4.dp))
+    ExposedDropdownMenuBox(
+        expanded = ouvert.value,
+        onExpandedChange = {
+            ouvert.value = !ouvert.value
+        }) {
+        TextField(
+            value = selectedOptionText.value,
+            onValueChange = {},
+            modifier = Modifier
+                .width(350.dp)
+                .menuAnchor(),
+            readOnly = true,
+            label = { Text(text = "Ville: ")},
+            shape = RoundedCornerShape(20.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = ouvert.value)
+            }
+            )
+        ExposedDropdownMenu(
+            expanded = ouvert.value,
+            onDismissRequest = { ouvert.value = false
+            }
+        ) {
+            liste.forEach { selectionOption ->
+                DropdownMenuItem(
+                    text = { Text(text = selectionOption) },
+                    onClick = {
+                        selectedOptionText.value = selectionOption
+                        ouvert.value = false
+                    })
             }
         }
     }
-
-}
-
-@Composable
-fun BoutonList(ouvert: Boolean,
-               onClick: () -> Unit,
-               modifier: Modifier = Modifier
-){
-    IconButton(
-        onClick = onClick,
-        modifier = modifier
-    ){
-        Icon(
-        imageVector = if (ouvert) Icons.Filled.ExpandLess else Icons
-            .Filled.ExpandMore,
-        contentDescription = "Ouvrir"
-    )
-    }
-
 }
 
 @RequiresApi(Build.VERSION_CODES.M)
