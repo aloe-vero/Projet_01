e2737e3a0b682ef55d73ebf9a23b8abca416b87a
eam766 <albertemilie03@gmail.com>
2024-03-24 20:53:20 -0400

    Icon search

--- app/src/main/java/com/example/projet_01/MainActivity.kt
+++ app/src/main/java/com/example/projet_01/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.projet_01
 
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -31,6 +39,7 @@ import java.util.Date
 import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -48,10 +57,13 @@ class MainActivity : ComponentActivity() {
 }
 
 //Englobe tout l'affichage de l'application
+@RequiresApi(Build.VERSION_CODES.M)
 @Composable
 fun DisplayWeather(modifier: Modifier = Modifier) {
     val currentDate: String = SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).format(Date())
-    Column(modifier = modifier.fillMaxSize(),
+    Column(modifier = modifier
+        .fillMaxSize()
+        .background(Color.LightGray),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,) {
         Spacer(Modifier.height(20.dp))
@@ -72,7 +84,7 @@ fun DisplayWeather(modifier: Modifier = Modifier) {
             modifier = modifier
         )
         Spacer(Modifier.height(20.dp))
-        Icon()
+        IconWeather()
         Spacer(Modifier.height(10.dp))
         Text(
             text = "peu nuageux",
@@ -81,12 +93,13 @@ fun DisplayWeather(modifier: Modifier = Modifier) {
     }
 }
 @Composable
-fun Icon(modifier: Modifier = Modifier){
+fun IconWeather(modifier: Modifier = Modifier){
     Image(
         painter = painterResource(R.drawable.icon_peu_nuageux),
         contentDescription = "weather"
     )
 }
+@RequiresApi(Build.VERSION_CODES.M)
 @Composable
 fun SearchBar(modifier: Modifier = Modifier){
     TextField(
@@ -98,6 +111,12 @@ fun SearchBar(modifier: Modifier = Modifier){
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent
         ),
+        leadingIcon = {
+           Icon(
+               imageVector = Icons.Filled.Search,
+               contentDescription = "search"
+           )
+        },
         trailingIcon={
             androidx.compose.material3.Icon(
                 Icons.Default.Clear,
@@ -111,6 +130,13 @@ fun SearchBar(modifier: Modifier = Modifier){
     )
 }
 
+@Composable
+fun ListCity(modifier: Modifier = Modifier){
+    val liste : List<String> = listOf("Montréal", "Laval", "Québec", "Position actuelle")
+
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
